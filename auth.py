e4b579b0a6f93c9ca3c3422d58b7868939bee5f9
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2023-10-10 22:11:41 +0530

    Bump to v1.6

--- auth.py
+++ auth.py
@@ -1,28 +1,16 @@
-from flask import Flask, request, redirect, Response
-from sys import argv, exit as sexit
-from httpx import Client as httpx_client
-from webbrowser import open as open_link
-from logging import (
-    getLogger,
-    basicConfig,
-    FileHandler,
-    StreamHandler,
-    INFO,
-    WARNING
-)
+import uvicorn
 
-basicConfig(
-    level=INFO,
-    datefmt='%d/%m/%Y %H:%M:%S',
-    format='[%(asctime)s][%(name)s][%(levelname)s] -> %(message)s',
-    handlers=[FileHandler('event-log.txt'), StreamHandler()]
-)
-logger = getLogger('auth_client')
-getLogger('werkzeug').setLevel(WARNING)
+from quart import Quart, request, redirect, Response
+from sys import argv, exit as sys_exit
+from httpx import AsyncClient as httpx_client
+from webbrowser import open as open_link
+from logging import getLogger
+
+from config import LOGGER_CONFIG_JSON
 
 if len(argv) < 3:
-    logger.error("Error: You must pass both the client ID and secret as command line arguments.")
-    sexit(1)
+    print("Error: You must pass both the client ID and secret as command line arguments.")
+    sys_exit(1)
 
 SCOPES = [
     "Directory.Read.All",
@@ -51,40 +39,48 @@ auth_endpoint = (
 )
 token_url = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
 
-web_server = Flask('auth_client')
+web_server = Quart('auth_client')
 web_client = httpx_client()
 
+@web_server.before_serving
+async def before_serve() -> None:
+    logger = getLogger('uvicorn')
+    logger.info('If your browser does not open automatically go to the following link:\nhttp://127.0.0.1:53682/auth')
+    open_link('http://127.0.0.1:53682/auth', new=1)
+
 @web_server.errorhandler(400)
-def invalid_request(_=None) -> Response:
+async def invalid_request(_=None) -> Response:
     return 'Invalid request.', 400
 
 @web_server.errorhandler(404)
-def not_found(_) -> Response:
+async def not_found(_) -> Response:
     return 'Resource not found.', 404
 
 @web_server.route('/')
-def callback() -> Response:
+async def callback() -> Response:
     code = request.args.get('code', None)
     if not code:
-        return invalid_request()
+        return await invalid_request()
     
-    resp = web_client.post(token_url, data={
+    resp = (await web_client.post(token_url, data={
         'client_id': argv[1],
         'client_secret': argv[2],
         'code': code,
         'redirect_uri': "http://localhost:53682",
         'grant_type': 'authorization_code'
-    }).json()
-    logger.info(resp)
+    })).json()
     
     return resp, 200
 
 @web_server.route('/auth')
-def auth() -> Response:
+async def auth() -> Response:
     return redirect(auth_endpoint)
     
-
 if __name__ == '__main__':
-    logger.info('If your browser does not open automatically go to the following link: http://127.0.0.1:53682/auth')
-    open_link('http://127.0.0.1:53682/auth', 1)
-    web_server.run(host='0.0.0.0', port=53682)
+    uvicorn.run(
+        app=web_server,
+        host='0.0.0.0',
+        port=53682,
+        log_config=LOGGER_CONFIG_JSON,
+        access_log=False
+    )
