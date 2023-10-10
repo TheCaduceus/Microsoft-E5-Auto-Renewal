47df5d9465a5db38ca8e756c0ddb7bca5dbf2718
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2023-10-10 21:41:16 +0530

    Bump to v1.6

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from flask import Flask, request, Response, send_file
-from threading import Thread
-from time import sleep
-from httpx import Client as httpx_client
+import uvicorn
+
+from quart import Quart, request, Response, send_file
+from asyncio import sleep as async_sleep
+from httpx import AsyncClient as httpx_client, Response as httpx_Response
 from random import shuffle
-from logging import (
-    getLogger, 
-    basicConfig,
-    FileHandler, 
-    StreamHandler,
-    INFO,
-    WARNING
-)
+from logging import getLogger
 
 from config import (
     REFRESH_TOKEN,
@@ -19,23 +13,15 @@ from config import (
     WEB_APP_PASSWORD,
     WEB_APP_HOST,
     WEB_APP_PORT,
-    TIME_DELAY
+    TIME_DELAY,
+    LOGGER_CONFIG_JSON
 )
 
-basicConfig(
-    level=INFO,
-    datefmt='%d/%m/%Y %H:%M:%S',
-    format='[%(asctime)s][%(name)s][%(levelname)s] -> %(message)s',
-    handlers=[FileHandler('event-log.txt'), StreamHandler()]
-)
-logger = getLogger('main')
-getLogger('werkzeug').setLevel(WARNING)
-
-web_server = Flask(__name__)
+web_server = Quart(__name__)
 web_client = httpx_client()
 
-server_stats = {'version': 1.5,'totalRequests': 0, 'totalSuccess': 0, 'totalErrors': 0}
-auth_endpoint= 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+server_stats = {'version': 1.6,'totalRequests': 0, 'totalSuccess': 0, 'totalErrors': 0}
+token_endpoint = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
 endpoints = [
     'https://graph.microsoft.com/v1.0/me/drive/root',
     'https://graph.microsoft.com/v1.0/me/drive',
@@ -57,16 +43,11 @@ endpoints = [
     'https://graph.microsoft.com/v1.0/sites/root/drives'
 ]
 
-def acquire_access_token(refresh_token:str|None=None) -> str:
-    """
-    Acquires the access token for the web client by making a POST request to the authentication endpoint with the provided data.
+async def log_response(response:httpx_Response) -> None:
+    request = response.request
+    logger.info(f'HTTP Request: {request.method} {request.url} {response.http_version} {response.status_code} {response.reason_phrase}')
 
-    Args:
-        refresh_token (str | None): The refresh token used to acquire the access token. If None, the default refresh token will be used.
-
-    Returns:
-        str: The access token.
-    """
+async def acquire_access_token(refresh_token:str|None=None) -> str:
     web_client.headers.update({'Content-Type': 'application/x-www-form-urlencoded'})
     data = {
         'grant_type': 'refresh_token',
@@ -76,18 +57,9 @@ def acquire_access_token(refresh_token:str|None=None) -> str:
         'redirect_uri': 'http://localhost:53682/'
     }
 
-    return web_client.post(auth_endpoint, data=data).json()['access_token']
+    return (await web_client.post(token_endpoint, data=data)).json()['access_token']
 
-def call_endpoints(access_token:str) -> None:
-    """
-    Shuffles the endpoints list, updates the headers of the web client with the provided access token, and makes GET requests to each endpoint in the shuffled list with a delay between each request.
-
-    Args:
-        access_token (str): The access token used for authorization.
-
-    Returns:
-        None
-    """
+async def call_endpoints(access_token:str) -> None:
     shuffle(endpoints)
     web_client.headers.update(
         {
@@ -97,44 +69,44 @@ def call_endpoints(access_token:str) -> None:
     )
 
     for endpoint in endpoints:
-        sleep(TIME_DELAY)
+        await async_sleep(TIME_DELAY)
         try:
-            web_client.get(endpoint)
+            await web_client.get(endpoint)
         except Exception:
             pass
 
-@web_server.route("/")
-def home() -> Response:
-    return server_stats, 200
+@web_server.before_serving
+async def before_serve() -> None:
+    global logger
+    logger = getLogger('uvicorn')
+    web_client.event_hooks['response'] = [log_response]
+    logger.info("Server is now started!")
+    host = f"127.0.0.1:{WEB_APP_PORT}" if WEB_APP_HOST == "0.0.0.0" else f"{WEB_APP_HOST}:{WEB_APP_PORT}"
+    logger.info(f'Server running on {host}')
+
+@web_server.after_serving
+async def after_serve() -> None:
+    logger.info('Server is now stopped!')
+
+@web_server.before_request
+async def before_request() -> None:
+    server_stats['totalRequests'] += 1
 
 @web_server.after_request
-def requests_counter(response:Response):
-    server_stats['totalRequests'] += 1
+async def after_request(response:Response) -> Response:
     if response.status_code == 201:
         server_stats['totalSuccess'] += 1
     elif response.status_code >= 400:
         server_stats['totalErrors'] += 1
     return response
 
-@web_server.errorhandler(400)
-def invalid_request(_) -> Response:
-    return 'Invalid request.', 400
-
-@web_server.errorhandler(404)
-def not_found(_) -> Response:
-    return 'Resource not found.', 404
-
-@web_server.errorhandler(405)
-def invalid_method(_) -> Response:
-    return 'Invalid method.', 405
-
-@web_server.errorhandler(415)
-def no_data(_) -> Response:
-    return 'No json data passed.', 415
+@web_server.route("/")
+async def home() -> Response:
+    return server_stats, 200
 
 @web_server.route('/call', methods=['POST'])
-def run_executor() -> Response:
-    json_data = request.json
+async def create_task() -> Response:
+    json_data = await request.json
     password = json_data.get('password')
     if not password:
         return 'Password is required to use web app.', 401
@@ -142,15 +114,14 @@ def run_executor() -> Response:
         return 'Access denied - invalid password.', 403
     
     refresh_token = json_data.get('refresh_token')
-    access_token = acquire_access_token(refresh_token)
+    access_token = await acquire_access_token(refresh_token)
+
+    web_server.add_background_task(call_endpoints, access_token)
     
-    executor = Thread(target=call_endpoints, args=[access_token])
-    executor.start()
-    
-    return 'Success - new thread created.', 201
+    return 'Success - new task created.', 201
 
 @web_server.route('/logs')
-def send_logs() -> Response:
+async def send_logs() -> Response:
     password = request.args.get('password')
     as_file = request.args.get('as_file', 'False') in {'TRUE','True','true'}
     if not password:
@@ -158,7 +129,29 @@ def send_logs() -> Response:
     if password != WEB_APP_PASSWORD:
         return 'Access denied - invalid password.', 403
     
-    return send_file('event-log.txt', as_attachment=as_file)
+    return await send_file('event-log.txt', as_attachment=as_file)
+
+@web_server.errorhandler(400)
+async def invalid_request(_) -> Response:
+    return 'Invalid request.', 400
+
+@web_server.errorhandler(404)
+async def not_found(_) -> Response:
+    return 'Resource not found.', 404
+
+@web_server.errorhandler(405)
+async def invalid_method(_) -> Response:
+    return 'Invalid method.', 405
+
+@web_server.errorhandler(415)
+async def no_data(_) -> Response:
+    return 'No json data passed.', 415
 
 if __name__ == '__main__':
-    web_server.run(host=WEB_APP_HOST, port=WEB_APP_PORT)
+    uvicorn.run(
+        app="main:web_server",
+        host=WEB_APP_HOST,
+        port=WEB_APP_PORT,
+        log_config=LOGGER_CONFIG_JSON,
+        access_log=False
+    )
